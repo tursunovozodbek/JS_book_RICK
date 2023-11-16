279b2fda8c5ddb97f65e90770b67aa291bd14a84
Ozodbek <tursunovozodbek505@gmail.com>
2023-11-17 00:06:27 +0500

    javaScript from zero to hero

---
+++ chapter4/1/lecture1.js
@@ -0,0 +1,15 @@
+// let VS var variable declaration
+// VAR
+var a = 3;
+var a = 4;
+var a = 5;
+// LET
+let b = 3;
+b = 4;
+b = 5;
+
+// const x = 5;
+
+// const y = 3;
+
+// console.log(x * y);
---
+++ chapter4/5/doWhile.js
@@ -0,0 +1,7 @@
+//Do while, the code will be execuetd regardless of the confition
+
+let i = 12;
+
+do {
+  console.log("The value of i:", i);
+} while (i < 10);
